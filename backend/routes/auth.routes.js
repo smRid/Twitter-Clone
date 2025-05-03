e9c876d6cb56090aa1d085f86d4f2534016971e4
smRid <omorsk911@gmail.com>
2025-05-03 12:03:10 +0600

    Auth controllers added

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,14 +1,12 @@
 import express from 'express';
-import { signup, login, logout } from '../controllers/auth.controller.js';
+import { signup, login, logout, getMe } from '../controllers/auth.controller.js';
+import { protectRoute } from '../middleware/protectRoute.js';
 
 const router = express.Router();
 
 router.post("/signup", signup);
-
 router.post("/login", login);
-
 router.post("/logout", logout);
-
-
+router.get("/me", protectRoute, getMe); // Ensure this is a GET route
 
 export default router;
