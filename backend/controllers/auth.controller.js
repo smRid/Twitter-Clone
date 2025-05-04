cf34f3bc1f89b73678d83a56985f53db5d0caa9b
smRid <omorsk911@gmail.com>
2025-05-04 08:01:21 +0600

    users routes & controllers added

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -58,9 +58,6 @@ export const signup = async (req, res) => {
 	}
 };
 
-
-
-
 export const login = async (req, res) => {
 	try {
 		const { username, password } = req.body;
--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,33 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import { v2 as cloudinary } from "cloudinary";
 
-import authRoutes from './routes/auth.routes.js'; 
-import connectMongoDB from './db/connectMongoDB.js';
+import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
 
+import connectMongoDB from "./db/connectMongoDB.js";
 
 dotenv.config();
+
+cloudinary.config({
+	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json()); //to parse res.body
-app.use(express.urlencoded({ extended: true })); //to parse from data(urlencoded)
+app.use(express.json()); // to parse req.body
+app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
 
-app.use(cookieParser()); 
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    connectMongoDB();
+	console.log(`Server is running on port ${PORT}`);
+	connectMongoDB();
 });
