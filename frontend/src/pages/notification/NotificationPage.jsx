e18b52eed7f0ef382b5ee4d34914f7a9ea9c0581
smRid <omorsk911@gmail.com>
2025-05-07 00:13:50 +0600

    notification page bug fix

--- frontend/src/pages/notification/NotificationPage.jsx
+++ frontend/src/pages/notification/NotificationPage.jsx
@@ -52,13 +52,13 @@ const NotificationPage = () => {
 			<div className='flex-[4_4_0] border-l border-r border-gray-700 min-h-screen'>
 				<div className='flex justify-between items-center p-4 border-b border-gray-700'>
 					<p className='font-bold'>Notifications</p>
-					<div className='dropdown '>
+					<div className='dropdown relative'>
 						<div tabIndex={0} role='button' className='m-1'>
 							<IoSettingsOutline className='w-4' />
 						</div>
 						<ul
 							tabIndex={0}
-							className='dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52'
+							className='dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52 absolute right-2 top-12 md:right-0 md:top-auto'
 						>
 							<li>
 								<a onClick={deleteNotifications}>Delete all notifications</a>
