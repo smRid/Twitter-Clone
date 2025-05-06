2ed01aa18b558cd133e9a7e0d2cf25dabdafae64
smRid <omorsk911@gmail.com>
2025-05-06 20:41:54 +0600

    comment error fix

--- frontend/src/utils/date/index.js
+++ frontend/src/utils/date/index.js
@@ -1,69 +1,42 @@
-import Post from "./Post";
-import PostSkeleton from "../skeletons/PostSkeleton";
-import { useQuery } from "@tanstack/react-query";
-import { useEffect } from "react";
+export const formatPostDate = (createdAt) => {
+	const currentDate = new Date();
+	const createdAtDate = new Date(createdAt);
 
-const Posts = ({ feedType }) => {
-	const getPostEndpoint = () => {
-		switch (feedType) {
-			case "forYou":
-				return "/api/posts/all";
-			case "following":
-				return "/api/posts/following";
-			default:
-				return "/api/posts/all";
-		}
-	};
+	const timeDifferenceInSeconds = Math.floor((currentDate - createdAtDate) / 1000);
+	const timeDifferenceInMinutes = Math.floor(timeDifferenceInSeconds / 60);
+	const timeDifferenceInHours = Math.floor(timeDifferenceInMinutes / 60);
+	const timeDifferenceInDays = Math.floor(timeDifferenceInHours / 24);
 
-	const POST_ENDPOINT = getPostEndpoint();
-
-	const {
-		data: posts,
-		isLoading,
-		refetch,
-		isRefetching,
-	} = useQuery({
-		queryKey: ["posts"],
-		queryFn: async () => {
-			try {
-				const res = await fetch(POST_ENDPOINT);
-				const data = await res.json();
-
-				if (!res.ok) {
-					throw new Error(data.error || "Something went wrong");
-				}
-
-				return data;
-			} catch (error) {
-				throw new Error(error);
-			}
-		},
-	});
-
-	useEffect(() => {
-		refetch();
-	}, [feedType, refetch]);
-
-	return (
-		<>
-			{(isLoading || isRefetching) && (
-				<div className='flex flex-col justify-center'>
-					<PostSkeleton />
-					<PostSkeleton />
-					<PostSkeleton />
-				</div>
-			)}
-			{!isLoading && !isRefetching && posts?.length === 0 && (
-				<p className='text-center my-4'>No posts in this tab. Switch 👻</p>
-			)}
-			{!isLoading && !isRefetching && posts && (
-				<div>
-					{posts.map((post) => (
-						<Post key={post._id} post={post} />
-					))}
-				</div>
-			)}
-		</>
-	);
+	if (timeDifferenceInDays > 1) {
+		return createdAtDate.toLocaleDateString("en-US", { month: "short", day: "numeric" });
+	} else if (timeDifferenceInDays === 1) {
+		return "1d";
+	} else if (timeDifferenceInHours >= 1) {
+		return `${timeDifferenceInHours}h`;
+	} else if (timeDifferenceInMinutes >= 1) {
+		return `${timeDifferenceInMinutes}m`;
+	} else {
+		return "Just now";
+	}
 };
-export default Posts;
+
+export const formatMemberSinceDate = (createdAt) => {
+	const date = new Date(createdAt);
+	const months = [
+		"January",
+		"February",
+		"March",
+		"April",
+		"May",
+		"June",
+		"July",
+		"August",
+		"September",
+		"October",
+		"November",
+		"December",
+	];
+	const month = months[date.getMonth()];
+	const year = date.getFullYear();
+	return `Joined ${month} ${year}`;
+};
